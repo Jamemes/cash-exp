daac81af3247d24f4af210e98b9104324fce5b48
Jamemes <56838048+Jamemes@users.noreply.github.com>
2026-06-05 21:40:10 +0300

    Update experiencemanager.lua

--- experiencemanager.lua
+++ experiencemanager.lua
@@ -5,7 +5,7 @@ end
 local data = ExperienceManager.get_xp_dissected
 function ExperienceManager:get_xp_dissected(success, num_winners, personal_win)
 	local xp, tbl = data(self, success, num_winners, personal_win)
-	local cash_exp = tbl.job_xp ~= 0 and offshore_rate_exp(managers.money:get_potential_payout_from_current_stage()) or 0
+	local cash_exp = managers.job:stage_success() and managers.job:on_last_stage() and offshore_rate_exp(managers.money:get_potential_payout_from_current_stage()) or 0
 	for id, value in pairs(tbl) do
 		if type(value) == "number" and id ~= "stage_xp" then
 			tbl[id] = 0
@@ -20,14 +20,13 @@ end
 local data = ExperienceManager.get_contract_xp_by_stars
 function ExperienceManager:get_contract_xp_by_stars(job_id, job_stars, risk_stars, professional, job_days, extra_params)
 	local total_payout_min, base_payout, risk_payout = managers.money:get_contract_money_by_stars(job_stars, risk_stars, job_days, job_id)
-	local xp, tbl = data(self, job_id, job_stars, risk_stars, professional, job_days, extra_params)
+	local _, tbl = data(self, job_id, job_stars, risk_stars, professional, job_days, extra_params)
 
 	for id, value in pairs(tbl) do
 		tbl[id] = 0
 	end
 
-	tbl[1] = offshore_rate_exp(risk_payout[1])
-	tbl[2] = offshore_rate_exp(risk_payout[3])
-
+	tbl[1] = offshore_rate_exp(type(risk_payout) == "table" and risk_payout[1] or base_payout)
+	tbl[2] = offshore_rate_exp(type(risk_payout) == "table" and risk_payout[3] or risk_payout)
 	return offshore_rate_exp(total_payout_min), tbl
 end
